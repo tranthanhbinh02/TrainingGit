9766ea1d8c6e19cf5ddd143cf64819297cc78549
tranthanhbinh02 <166909259+tranthanhbinh02@users.noreply.github.com>
2024-07-15 10:15:38 +0700

    Update bai2.c

--- No1/Task2/bai2.c
+++ No1/Task2/bai2.c
@@ -1,4 +1,4 @@
-/*write a progaram to print the contents of a text file to the console, with the filr path set within the code*/
+/*write a program to print the contents of a text file to the console, with the filr path set within the code*/
 
 #include <stdio.h>
 #include <math.h>
