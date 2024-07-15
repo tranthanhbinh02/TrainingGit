dd436bbdc6393d3746518004a1be4e25b0f50b96
tranthanhbinh02 <166909259+tranthanhbinh02@users.noreply.github.com>
2024-07-15 17:24:32 +0700

    Update task.c

--- No5/task/task.c
+++ No5/task/task.c
@@ -8,13 +8,13 @@
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+        return -1;
     }
 
     FILE *file = fopen(argv[1], "r");
     if (file == NULL) {
         perror("Error opening file");
-        return 1;
+        return -1;
     }
 
     int ch;
