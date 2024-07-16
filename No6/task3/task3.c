c36c46ef2a576461143c5d5c2005209ffcb2e4a7
tranthanhbinh02 <166909259+tranthanhbinh02@users.noreply.github.com>
2024-07-16 17:08:20 +0700

    Update task3.c

--- No6/task3/task3.c
+++ No6/task3/task3.c
@@ -1,1 +1,31 @@
+/* Write a C program to block SIGINT, then check if SIGINT is in the pending
+signals, and if so, exit the program */
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(void) {
+    sigset_t newmask, oldmask, pendmask;
+
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGINT); /
+
+    /
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+        perror("SIG_BLOCK error");
+
+    while (1) {
+        
+        if (sigpending(&pendmask) < 0)
+            perror("sigpending error");
+
+        if (sigismember(&pendmask, SIGINT)) {
+            printf("\nSIGINT is pending\n");
+            break;
+        }
+    }
+
+    return 0;
+}
