2413e87e51784274f3eafe126b87ac66c508a6d3
tranthanhbinh02 <ttbinh12122002@gmail.com>
2024-07-30 10:16:39 +0700

    upload server

--- No9/task/server.c
+++ No9/task/server.c
@@ -7,34 +7,70 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
-#include <time.h>
 
-void main()
-{
-	int listenfd = -1;
-	int connfd = -1;
-	struct sockaddr_in server_addr;
-	char send_buffer[1024];
-	time_t ticks;
+int main() {
+    int listenfd = -1;
+    int connfd = -1;
+    struct sockaddr_in server_addr;
+    char recv_buffer[1024];
 
-	memset(send_buffer, 0, sizeof(send_buffer));
-	memset(&server_addr, 0, sizeof(server_addr));
+    // Initialize buffer
+    memset(recv_buffer, 0, sizeof(recv_buffer));
+    memset(&server_addr, 0, sizeof(server_addr));
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server_addr.sin_port = htons(5000);
+    // Create socket
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-	bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	listen(listenfd, 3);
+    // Set up server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(5000);
 
-	while(1) {
-		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-		printf("hihi");
-		ticks = time(NULL);
-		sprintf(send_buffer, "Server reply %s", ctime(&ticks));
-		write(connfd, send_buffer, strlen(send_buffer));
-		close(connfd);
-	}
-	close(listenfd);
+    // Bind socket to address
+    if (bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(listenfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(listenfd, 3) < 0) {
+        perror("listen failed");
+        close(listenfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port 5000...\n");
+
+    while (1) {
+        // Accept incoming connection
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        if (connfd < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        // Read data from client
+        ssize_t n = read(connfd, recv_buffer, sizeof(recv_buffer) - 1);
+        if (n > 0) {
+            recv_buffer[n] = '\0'; // Null-terminate the received data
+            printf("Received from client: %s\n", recv_buffer);
+
+            // Send acknowledgment to client
+            const char *ack = "Message received!";
+            write(connfd, ack, strlen(ack));
+        }
+
+        // Close connection
+        close(connfd);
+    }
+
+    // Close listening socket
+    close(listenfd);
+    return 0;
 }
+
